9c469fff0be43d7a3b7540393b46c413d7a257ef
16126008 <16126008@L815WD220025.edu.cbe.ab.ca>
2022-03-28 10:18:30 -0600

    Updated

---
+++ Chapter5/src/DigitsSum.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+/*
+
+Program: DigitsSum.java      Last Date of this Revision: 
+
+Purpose: Create a DigitsSum application that prompts the user for the non-negative integer and then displays the sum of the digits. 
+
+Author: Ahmad Cheema, 
+School: CHHS
+Course: Computer Science  20
+ _
+
+*/
+public class DigitsSum
+{
+
+	public static void main(String[] args) 
+	{
+		Scanner input = new Scanner (System.in);
+		
+		System.out.print("Enter a positive integer:  ");//ask for a positive integer
+		
+		int numberMax = input.nextInt();//record positive integer
+		
+	}
+	
+}
