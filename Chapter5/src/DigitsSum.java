5a89b263bb446bbd43cad40886ad4417045cf9c2
16126008 <16126008@L815WD220025.edu.cbe.ab.ca>
2022-03-30 09:45:43 -0600

    Done

--- Chapter5/src/DigitsSum.java
+++ Chapter5/src/DigitsSum.java
@@ -1,7 +1,7 @@
 
 /*
 
-Program: DigitsSum.java      Last Date of this Revision: 
+Program: DigitsSum.java      Last Date of this Revision: March 30, 2022
 
 Purpose: Create a DigitsSum application that prompts the user for the non-negative integer and then displays the sum of the digits. 
 
@@ -20,12 +20,34 @@ public class DigitsSum
 
 	public static void main(String[] args) 
 	{
-		Scanner input = new Scanner (System.in);
+		int number, digit, sum = 0; 
 		
-		System.out.print("Enter a positive integer:  ");//ask for a positive integer
+		Scanner sc = new Scanner(System.in); 
 		
-		int numberMax = input.nextInt();//record positive integer
+		System.out.print("Enter a positive integer: ");  //ask for a positive integer
+		
+		number = sc.nextInt();
+		
+		
+		while(number > 0)  
+		{  
+		    
+		digit = number % 10;  //finds the last digit of the given number  
+		
+		sum = sum + digit;  //adds last digit to the variable sum 
+		  
+		number = number / 10;  //removes the last digit from the number  
+		}  
+		 
+		System.out.println("The sum of the cubes of the digits is: "+sum);  //prints the result  
 		
 	}
 	
 }
+
+/* Screen Dump
+
+Enter a positive integer: 892
+The sum of the cubes of the digits is: 19
+
+*/
