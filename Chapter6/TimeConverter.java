a2cd40e3124f92010c96c777d10941beafd5c596
16126008 <16126008@L815WD220025.edu.cbe.ab.ca>
2022-04-05 11:25:16 -0600

    Still Working on this

---
+++ Chapter6/TimeConverter.java
@@ -0,0 +1,14 @@
+
+
+/*
+
+Program: TimeConverter.java      Last Date of this Revision: April 5
+
+Purpose: Create a Evens application that displays the even numbers between 1 and 20, inclusive.
+
+Author: Ahmad Cheema, 
+School: CHHS
+Course: Computer Science  20
+ _
+
+*/
