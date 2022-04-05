bc4a4729f2eaaf8a55d6c5fb9b2ac5baebb7ce7b
16126008 <16126008@L815WD220025.edu.cbe.ab.ca>
2022-04-05 11:19:41 -0600

    Done

--- Chapter5/src/AccountSetup.java
+++ Chapter5/src/AccountSetup.java
@@ -21,62 +21,71 @@ public class AccountSetup
 
 public static void main(String[] args)  
   {
-     System.out.print("Enter a user name: ");//ask for user name
+    System.out.print("Enter a user name: ");//ask for user name
 
-      Scanner userna = new Scanner (System.in);
+    Scanner userna = new Scanner (System.in);
 
-      String username = userna.next();
+    String username = userna.next();
 
-      username = username.toLowerCase();//user name and password going to turn into lower case 
+    username = username.toLowerCase();//user name and password going to turn into lower case 
 
-      String userInput = "c";
+    String userInput = "q";
 
-        
+      
 
-         while (!(userInput.equalsIgnoreCase("q")))
-          {
+       while (!(userInput.equalsIgnoreCase("c")))
+        {
 
-             System.out.print("Enter a password that is at least 8 characters: ");//ask for password with at least 8 characters
-    
-                Scanner upass = new Scanner (System.in);
-                
-                String password = userna.next();
-                
-                password = password.toLowerCase();
-                    
-                int character = password.length();
-                   
-                   
+           System.out.print("Enter a password that is at least 8 characters: ");//ask for password with at least 8 characters
+  
+              Scanner upass = new Scanner (System.in);
+              
+              String password = userna.next();
+              
+              password = password.toLowerCase();
+                  
+              int character = password.length();
+                 
+                 
 	           if (character < 8)
 	            {
-	             System.out.println("Too short please enter a password with atleast 8 characters: ");//displays when password is less than 8 characters
+	             System.out.println("Way to short. ");//displays when password is less than 8 characters
 	              continue;
 	            }
 	           
 	           	
-           		System.out.print("Do you want to continue(c) or QUIT(q)");
-           		userInput = upass.next();
-           		
-           		if(userInput.equalsIgnoreCase("q"))
-           		{
-           		  System.out.println("Your username is " + username + " and your password is " +password);//displays user name and password in lower case
-           			
-           		}
-           		
-              
+         		System.out.print("Do you want to continue(c) with the application or QUIT(q) to quit and rewrite your password");
+         		userInput = upass.next();
+         		
+         		if(userInput.equalsIgnoreCase("c"))
+         		{
+         		  System.out.println("Your username is " + username + " and your password is " +password);//displays user name and password in lower case
+         			
+         		}
+         		
+            
 
-         }
-
-  }
+       }
 
 }
+
+
+
+}
+
+  
+
+
 
 /* Screen Dump
 
 Enter a user name: MattD
+Enter a password that is at least 8 characters: 123
+Way to short. 
 Enter a password that is at least 8 characters: Programmer7
-Do you want to continue(c) or QUIT(q)q
+Do you want to continue(c) with the application or QUIT(q) to quit and rewrite your passwordc
 Your username is mattd and your password is programmer7
+
 
 */
 
