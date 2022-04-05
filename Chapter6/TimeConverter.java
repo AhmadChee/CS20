a20ba88da7c676c7c04badab295d8a7a8a52f2ef
16126008 <16126008@L815WD220025.edu.cbe.ab.ca>
2022-04-05 11:26:41 -0600

    Still working on this

--- Chapter6/TimeConverter.java
+++ Chapter6/TimeConverter.java
@@ -4,7 +4,7 @@
 
 Program: TimeConverter.java      Last Date of this Revision: April 5, 2022
 
-Purpose: Create a TimeConverter application that allows the user to chosse among converting hours to minutes, days to hours, or hours into days. U
+Purpose: Create a TimeConverter application that allows the user to chosse among converting hours to minutes, days to hours, or hours into days. 
 
 Author: Ahmad Cheema, 
 School: CHHS
