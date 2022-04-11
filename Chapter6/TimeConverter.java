8de7ced9640eca0184d639eff042a1be339361af
16126008 <16126008@L815WD220025.edu.cbe.ab.ca>
2022-04-11 09:50:36 -0600

    Almost done TimeConverter. Moving on to next assignment

--- Chapter6/TimeConverter.java
+++ Chapter6/TimeConverter.java
@@ -1,67 +1,140 @@
-Scanner input = new Scanner (System.in);
+import java.utill.Scanner;
+/*
 
-System.out.print("Welcome to Time Converter! Please enter '1' to convert hours to minutes. '2' to convert minutes to hours. "
-+ "'3' to convert days to hours or '4' to convert hours to days: ");
+Program: TimeConverter.java      Last Date of this Revision: March 18, 2022
 
-NumberFormat nf = NumberFormat.getInstance();
+Purpose: Create a TimeConverte application that allows the user to choose among converting hours to minutes, days to hours, minutes to hours, or hours to days. Use methods as apprpriate. 
 
-int choice = input.nextInt();
+Author: Ahmad Cheema, 
+School: CHHS
+Course: Computer Science  20
+ _
+
+*/
+
+package timeconverter;
 
 
-       while (choice != -1)
-       
-     {
-       
-if (choice == 1)
+public class TimeConverter 
 {
-System.out.print("\nPlease enter how many hours you would like to convert to minutes: ");
-int h = input.nextInt();
+	public static void hoursTominutes()
+	{
+		double hTime, mTime;
+		
+		Scanner input = new Scanner (System.in);
+		
+		System.out.print("Enter a time in hours: ");
+		
+		hTime = input.nextDouble();
+		
+		mTime =  (hTemp * 60);
+		
+		System.out.print("The time in minutes is " + mTime);
+	}
+}
 
-double m = (double) h*60;
+public static void daysTohours()
+{
+	double dTIme, hTime;
+	
+	Scanner input = new Scanner(System.in);
+	
+	System.out.println("Enter the number of days:  ");
+	
+	dTime = input.nextDouble();
+	
+	hTime = (dTime * 24);
+	
+	System.out.println("The time in hours is " + hTime);
+}
 
-System.out.println("\n" +h +" hours converted to minutes is " +nf.format(m) +" minutes.");
+public static void minutesTohours()
+{
+	double mTime, hTime;
+	
+	Scanner input = new Scanner(System.in);
+	
+	System.out.println("Enter time in minutes: ");
+	
+	mTime = input.nextDouble();
+	
+	hTime = (mTime * 0.0166667);
+	
+	System.out.println("The time in hours is " + hTime);
+}
+
+public static void hoursTodays()
+{
+	double hTime, dTime;
+	
+	Scanner input = new Scanner(System.in);
+	
+	System.out.println("Enter time in hours: ");
+	
+	hTime = input.nextDouble();
+	
+	dTime = (hTime * 0.0416667);
+	
+	System.out.println("The time in days will be" + dTime)
 }
 
 
-else if (choice == 2)
 
+
+
+
+
+
+
+
+
+
+
+
+
+public static void main(String[] args)
 {
-System.out.print("\nPlease enter how many minutes you would like to convert to hours: ");
-int m2 = input.nextInt();
+	int choice;
+	
+	Scanner input = new Scanner (System.in);
+	
+	System.out.println("1. Hours to Minutes conversion.");
+	System.out.println("2. Days to Hours conversion.");
+	System.out.println("3. Minutes to Hours conversion.");
+	System.out.println("4. Hours to Days conversion.");
+	
+	
+	System.out.println("Enter your conversionn choice:  ");
+	
+	choice = input.nextInt();
+	
+	if(choice == 1)
+	{
+		hoursTominutes();
+	}
+	else if(choice == 2)
+	{
+		daysTohours();
+	}
+	
+	else if(choice == 3) 
+	{
+		minutesTohours();
+	}
+	
+	else 
+	{
+		hoursTodays();	
+	}
+		
 
-double h2 = (double) m2/60;
 
-System.out.println("\n" +m2 +" minutes converted to hours is " + nf.format(h2) +" hours.");
-}
-
-
-else if (choice == 3)
-
-{
-System.out.print("\nPlease enter how many days you would like to convert to hours: ");
-int d = input.nextInt();
-
-double h3 = (double) d*24;
-
-System.out.println("\n" +d +" days converted to hours is " +nf.format(h3) +" hours.");
-}
+	
+    
 
 
-else if (choice == 4)
+/* Screen Dump
 
-{
-System.out.print("\nPlease enter how many hours you would like to convert to days: ");
-int h4 = input.nextInt();
 
-double d1 = (double) h4/24;
 
-System.out.print("\n" +h4 +" hours converted to days is " +nf.format(d1) +" days.");
-}
-
-System.out.println("\n\nTo convert more, enter '1' to convert hours to minutes. '2' to convert minutes to hours. "
-+ "'3' to convert days to hours or '4' to convert hours to days or enter '-1' to end the program: ");
-
-choice = input.nextInt();
-
-    }
-       System.out.print("\nThank you for using TimeConverter, have a nice day!")
+*/
