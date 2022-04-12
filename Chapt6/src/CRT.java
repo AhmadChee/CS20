19470a8a5fae99bf484e9bda176d0d9b69c1b26e
16126008 <16126008@L815WD220025.edu.cbe.ab.ca>
2022-04-12 11:53:14 -0600

    Almost Done CRT

---
+++ Chapt6/src/CRT.java
@@ -0,0 +1,40 @@
+/*
+
+Program: CRT.java      Last Date of this Revision: 
+
+Purpose: CRT Page 143 Questions 2-7
+
+Author: Ahmad Cheema, 
+School: CHHS
+Course: Computer Science  20
+ .
+
+*/
+
+
+public class CRT 
+{
+/*
+
+2. The method declaration defines all the methods attributes, such as access level, return
+type, name, and arguments. The method body however is where all of the action takes place. 
+It contains the instruction that allow the implementation of the method.
+
+3. The public keyword is an access modifier. It is used to set the access level for methods, 
+classes, attributes and the constructors. 
+
+4. The keyword in the declaration of a method that states the access level of a method. 
+
+5. 
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ */
+	
+	
+}
