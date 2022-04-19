0d5913310d7bbd83ae907af8c76f08cf4d87fa7c
16126008 <16126008@L815WD220025.edu.cbe.ab.ca>
2022-04-19 11:52:20 -0600

    Working on Circle

---
+++ Chapter7/src/TestCircle.java
@@ -0,0 +1,14 @@
+
+public class TestCircle 
+{
+
+	public static void main(String[] args) 
+	{
+		Circle o = new Circle (3);
+		
+		
+		System.out.println("Circle has a radius " + o.getRadius());
+
+	}
+
+}
