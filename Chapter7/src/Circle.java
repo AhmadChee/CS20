7f23ece5f834496e1529ef349d8c0cc2d6a8ca9f
16126008 <16126008@L815WD220025.edu.cbe.ab.ca>
2022-04-19 11:52:06 -0600

    Working on Circle

---
+++ Chapter7/src/Circle.java
@@ -0,0 +1,38 @@
+
+
+
+public class Circle 
+{
+
+	private double radius;
+	
+	public Circle()//constructor method
+	{
+		radius = 1; //default radius
+				
+	   
+	}
+	
+	public Circle(double r)//constructor method2
+	{
+		radius = r;
+		
+		
+	}
+	
+	public void setRadius(double newRadius)
+	{
+		radius = newRadius;
+	}
+	
+	
+	public double getRadius()
+
+	{
+		return(radius);
+	}
+	
+	
+	
+
+}
