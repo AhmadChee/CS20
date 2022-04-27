b889aee165834b3eb7af9d615ddfa14cdcfccc28
16126008 <16126008@L815WD220025.edu.cbe.ab.ca>
2022-04-27 08:53:04 -0600

    Updated code

--- Chapter7/src/TestCircle.java
+++ Chapter7/src/TestCircle.java
@@ -6,9 +6,14 @@ public class TestCircle
 	{
 		Circle spot = new Circle();
 		
+		Circle c2 = new Circle(4);
+		
 		spot.setRadius(3);
 		System.out.println("Circle radius " + spot.getRadius());
 		System.out.println("Circle circumference: " + spot.circumference(3));
+		Circle.displayAreaformula();
+		
+		
 		
 		
 		
