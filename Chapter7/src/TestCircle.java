a3316c25e2c4827ea879ee27e089526eee2e0483
16126008 <16126008@L815WD220025.edu.cbe.ab.ca>
2022-04-27 09:44:38 -0600

    Updated

--- Chapter7/src/TestCircle.java
+++ Chapter7/src/TestCircle.java
@@ -4,14 +4,39 @@ public class TestCircle
 
 	public static void main(String[] args) 
 	{
-		Circle spot = new Circle();
+		Circle spot1 = new Circle();
 		
-		Circle c2 = new Circle(4);
+		Circle spot2 = new Circle(4);
 		
-		spot.setRadius(3);
-		System.out.println("Circle radius " + spot.getRadius());
-		System.out.println("Circle circumference: " + spot.circumference(3));
-		Circle.displayAreaformula();
+		spot1.setRadius(3);
+		System.out.println("Circle radius " + spot1.getRadius());
+		System.out.println("Circle circumference: " + spot1.circumference(3));
+		spot1.displayAreaformula();
+		
+		
+		System.out.println("\n \n \n");
+		
+		
+		
+		spot2.setRadius(4);
+		System.out.println("Circle radius " + spot2.getRadius());
+		System.out.println("Cirlce circumference: " + spot2.circumference(4));
+		spot2.displayAreaformula();
+		
+		System.out.println("\n \n \n");
+		
+		
+		if (spot1.equals(spot2))
+		{
+			System.out.println("Objects are equal.");
+		}
+		
+		else 
+		{
+			System.out.println("Objects are not equal.");
+		}
+		
+		
 		
 		
 		
