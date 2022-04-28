8179c7a608e38d5771700c61e4de2f935cc85652
16126008 <16126008@L815WD220025.edu.cbe.ab.ca>
2022-04-28 11:14:09 -0600

    Done

--- Chapter7/src/Circle.java
+++ Chapter7/src/Circle.java
@@ -1,51 +1,72 @@
+/*
+ 
+Program: Circle.java      Last Date of this Revision: April 28
+ 
+ 
+Purpose: Modify the circle class to include a member method 
+named circumference. The circumference() method should return 
+the circumference of the circle (2*PI*r). Include an overloaded 
+constructor that accepts the radius of the Circle object. Also 
+include a class named displayAreaformula. Override the equals()
+and toString() method. 
+ 
+Author: Ahmad Cheema, 
+School: CHHS
+Course: Computer Science  20
+ 
 
+*/
 
 
 public class Circle 
 {
 
-	public static final double PI = 3.14;
+	public static final double PI = 3.14;// PI number value 
 	
 	private double radius;
 	
+	
 	public Circle()//constructor method
-	{
+	 {
 		radius = 1; //default radius			   
-	}
+	 }
 	
 	public Circle(double r)//constructor method2
-	{
+	 {
 		radius = r;
-	}
+	 }
 	
 	public void setRadius(double newRadius)
-	{
+	 {
 		radius = newRadius;
-	}
+	 }
 	
 	
-	public double getRadius()  
-	{
+	public double getRadius()
+	 {
 		return(radius);
-	}
+	 }
 	
 	
-	public double circumference(double r)
-	{
+	public double circumference(double r)//member method circumference 
+	 {
 		
-		return( 2*PI*r);
-	}
+		return( 2*PI*r);//formula for circumference of a circle
+	 }
 	
-	public double area(double r)
-	{
-		return(PI*(r*r));
-	}
+	public double area(double r)//method for area of a circle
+	 {
+		return(PI*(r*r));//formula for area of a circle
+	 }
 	
-	public static void displayAreaformula()
-	{
+	public static void displayAreaformula()//class method displayAreaformula
+	 {
 		System.out.println("The formula for the area of a circle is a=Pi*r*r");
-	}
+	 }
 	
 	
 
 }
+
+
+//Screen Dump on Test Circle
--- Chapter7/src/TestCircle.java
+++ Chapter7/src/TestCircle.java
@@ -1,48 +1,68 @@
+// Header in Circle 
+
 
 public class TestCircle 
 {
 
 	public static void main(String[] args) 
 	{
-		Circle spot1 = new Circle();
+		Circle spot1 = new Circle();//first circle object
 		
-		Circle spot2 = new Circle(4);
+		Circle spot2 = new Circle(4);//second circle object
 		
-		spot1.setRadius(3);
-		System.out.println("Circle radius " + spot1.getRadius());
-		System.out.println("Circle circumference: " + spot1.circumference(3));
-		System.out.println("Circle area " + spot1.area(3));
-		spot1.displayAreaformula();
+		spot1.setRadius(3);//first circle object
+		System.out.println("Circle radius " + spot1.getRadius());//displays circle radius
+		System.out.println("Circle circumference: " + spot1.circumference(3));//displays circle circumference
+		System.out.println("Circle area " + spot1.area(3));//displays circle area
+		spot1.displayAreaformula();//displays formula for area of a circle
 		
 		
 		System.out.println("\n \n \n");
 		
-		spot2.setRadius(4);
-		System.out.println("Circle radius " + spot2.getRadius());
-		System.out.println("Cirlce circumference: " + spot2.circumference(4));
-		System.out.println("Circle area " + spot2.area(4));
-		spot2.displayAreaformula();
+		spot2.setRadius(4);//second circle object
+		System.out.println("Circle radius " + spot2.getRadius());//displays circle radius
+		System.out.println("Circle circumference: " + spot2.circumference(4));//displays circle circumference 
+		System.out.println("Circle area " + spot2.area(4));//displays circle area
+		spot2.displayAreaformula();//displays formula for area of a circle
+		
 		
 		System.out.println("\n \n \n");
 		
 		
 		if (spot1.equals(spot2))
 		{
-			System.out.println("Objects are equal.");
+			System.out.println("Objects are equal.");//displays this when the objects are equal
 		}
 		
 		else 
 		{
-			System.out.println("Objects are not equal.");
+			System.out.println("Objects are not equal.");//displays this when the objects are equal
 		}
-		
-		
-		
-		
-		
-		
-		
 		
 	}
 
 }
+
+
+/* Screen Dump
+ * 
+Circle radius 3.0
+Circle circumference: 18.84
+Circle area 28.26
+The formula for the area of a circle is a=Pi*r*r
+
+ 
+ 
+
+Circle radius 4.0
+Circle circumference: 25.12
+Circle area 50.24
+The formula for the area of a circle is a=Pi*r*r
+
+ 
+ 
+
+Objects are not equal.
+
+
+*/
