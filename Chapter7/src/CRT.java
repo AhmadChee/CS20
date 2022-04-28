84234c7587c6151b699e117656964f834a2aaadf
16126008 <16126008@L815WD220025.edu.cbe.ab.ca>
2022-04-28 11:42:43 -0600

    Working on CRT

---
+++ Chapter7/src/CRT.java
@@ -0,0 +1,41 @@
+/*
+ 
+Program: CRT.java      Last Date of this Revision: 
+ 
+ 
+Purpose: CRT Question #1-6 Page 175
+ 
+Author: Ahmad Cheema, 
+School: CHHS
+Course: Computer Science  20
+ 
+
+*/
+
+
+public class CRT 
+{
+/*
+
+1. 
+
+2. 
+
+3. 
+
+4. 
+
+5. a.
+   b. 
+   c.
+   d. 
+   e.
+   f.
+   g.
+
+6. 
+
+*/
+	
+	
+}
