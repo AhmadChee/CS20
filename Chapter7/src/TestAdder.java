d0d34f2ea52b5271bbdb1853fa522005698ed84a
16126008 <16126008@L815WD220025.edu.cbe.ab.ca>
2022-05-04 09:44:59 -0600

    Updated

--- Chapter7/src/TestAdder.java
+++ Chapter7/src/TestAdder.java
@@ -1,10 +1,32 @@
+import java.util.Scanner;
 
-public class TestAdder {
+public class TestAdder 
+{
 
 	public static void main(String[] args) 
 	{
+		int ans = 0;
+		int guess = 0;
+		int tries;
+		int score = 0;
+			
+		Player one = new Player (0, 0);
+		while (guess != 999) 
+		{
+			tries = one.getTry();
+			ans = one.getAns();
+			guess = one.getGuess();
+			switch (tries) 
+			{
+			case 1: score += 5; break;
+			case 2: score += 3; break;
+			case 3: score += 1; break;
+			case 4: score += 0;
+			System.out.println("Wrong answer. The correct answer is: " ); break;
+			}	
+		}
 		
-
+		System.out.print("Your Score is " + score);
 	}
 
 }
