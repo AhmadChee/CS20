7625c70581e8081a142ed6472dbef851d1a7f567
16126008 <16126008@L815WD220025.edu.cbe.ab.ca>
2022-05-04 09:44:27 -0600

    Updated

--- Chapter7/src/Adder.java
+++ Chapter7/src/Adder.java
@@ -1,47 +1,31 @@
 import java.util.Scanner;
 
-public class Adder {
+public class Adder 
+{
 
 	public static void main(String[] args) 
 	{
-		System.out.println("Choose one of the following operations: ");
-		
-		System.out.println("1. Addition");
-		System.out.println("2. Subtraction");
-		System.out.println("3. Multiplication");
-		System.out.println("4. Division");
-		
-		Scanner input = new Scanner (System.in);
-		System.out.println();
-		System.out.println("Enter \"1\", \"2\", \"3\" or \"4\"");
-		int operation = input.nextInt();
-		
-		
-		operation = input.nextInt();
-		
-		if(operation == 1)
+		int ans = 0;
+		int tries;
+		int score = 0;
+			
+		Player one = new Player (0, 0);
+		while (ans != 999) 
 		{
-			System.out.println("Addition");
+			tries = one.getTry();
+			ans = one.getAns();
+		
+			switch (tries) 
+			{
+			case 1: score += 5; break;
+			case 2: score += 3; break;
+			case 3: score += 1; break;
+			case 4: score += 0;
+			System.out.println("Wrong answer. The correct answer is: " + ans ); break;
+			}	
 		}
 		
-		else if(operation == 2);
-		{
-			System.out.println("Subtraction");
-		}
-		
-		if(operation == 3)
-		{
-			System.out.println("Multiplication");
-		}
-		
-		else if(operation == 4)
-		{
-			System.out.println("Division");
-		}
-		
-	
-				
-
+		System.out.print("Your Score is " + score);
 	}
 
 }
